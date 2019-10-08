c9ec96889df48f63e9f6243f5e91883ed4f678e3
nandani-chotara <nandani.chotara@gmail.com>
2019-10-08 11:38:57 -0400

    Created a basic application with the first screens
    Added:
    - SplashActivity
    - LoginActivity
    - MainActivity

---
+++ app/src/main/java/com/example/flashcards/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.example.flashcards;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+    public void onLoginClicked(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/flashcards/SplashActivity.java
@@ -0,0 +1,22 @@
+package com.example.flashcards;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+}
