bf5d29399cd9bee3c14e8fcb48bcc2b6a04588d1
nandani-chotara <nandani.chotara@gmail.com>
2019-10-20 19:47:21 -0400

    Added Recycler View Adapter for Deck Activity

--- app/src/main/java/com/example/flashcards/DeckActivity.java
+++ app/src/main/java/com/example/flashcards/DeckActivity.java
@@ -10,22 +10,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.View;
 
+import java.util.ArrayList;
+
 public class DeckActivity extends AppCompatActivity {
+
+    //vars
+    private ArrayList<String> mdeckNames = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.deck_fragment_container);
+        setContentView(R.layout.activity_deck);
 
-        FragmentManager fm = getSupportFragmentManager();
+       /* FragmentManager fm = getSupportFragmentManager();
         Fragment f = fm.findFragmentById(R.id.deck_fragment_container);
         if (f==null){
             f = new DeckFragment();
             fm.beginTransaction().add(R.id.deck_fragment_container, f).commit();
-        }
+        }*/
 
         /*Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -38,6 +45,35 @@ public class DeckActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });*/
+        initialise();
+    }
+
+    private void initialise(){
+        mdeckNames.add("CP104");
+        mdeckNames.add("CP106");
+        mdeckNames.add("CP164");
+        mdeckNames.add("CP264");
+        mdeckNames.add("CP213");
+        mdeckNames.add("CP212");
+        mdeckNames.add("CP470");
+        mdeckNames.add("CP312");
+        mdeckNames.add("CP372");
+        mdeckNames.add("CP372");
+        mdeckNames.add("CP372");
+        mdeckNames.add("CP372");
+        mdeckNames.add("CP372");
+        mdeckNames.add("CP372");
+        mdeckNames.add("CP372");
+        mdeckNames.add("CP372");
+
+
+        initRecyclerView();
+    }
+    private void initRecyclerView(){
+        RecyclerView recyclerView = findViewById(R.id.deck_recyclerView);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, mdeckNames);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
 }
