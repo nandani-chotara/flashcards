44e956d0807a939fa27bc24ac3145c3f02759951
EdmundLuii <edmund.lui1998@gmail.com>
2019-11-26 19:07:02 -0500

    couldnt get the dialog box going so just deleted it

--- app/src/main/java/com/example/flashcards/LoginActivity.java
+++ app/src/main/java/com/example/flashcards/LoginActivity.java
@@ -3,7 +3,6 @@ package com.example.flashcards;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Dialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -11,7 +10,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -26,18 +24,10 @@ public class LoginActivity extends AppCompatActivity {
     ImageButton btnLogin;
     EditText username;
     EditText password;
-    EditText firstName;
-    EditText lastNmae;
     FirebaseAuth mAuth;
 
     //register stuff
     Button btnRegister;
-    EditText rUsername;
-    EditText rPassword;
-    EditText rFirstName;
-    EditText rLastNmae;
-
-    Dialog myDialog;
 
     SharedPreferences sharedPreferences;
     SharedPreferences.Editor editor;
@@ -47,18 +37,17 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-
         username = findViewById(R.id.usernameInput);
         password = findViewById(R.id.passwordInput);
-        btnRegister = findViewById(R.id.registerBtn);
         btnLogin = findViewById(R.id.loginBtn);
+
+
+        btnRegister = findViewById(R.id.registerBtn);
 
         sharedPreferences = getSharedPreferences("loginref", MODE_PRIVATE);
         editor = sharedPreferences.edit();
 
         username.setText(sharedPreferences.getString("username", null));
-
-        myDialog = new Dialog(this);
 
     }
 
@@ -94,36 +83,16 @@ public class LoginActivity extends AppCompatActivity {
         this.startActivity(myIntent);
     }
 
-    public void signUp() {
-        Intent myIntent = new Intent(this, RegisterActivity.class);
-        this.startActivity(myIntent);
-    }
-
-    public void signUpButton(View view) {
-        TextView txtClose;
-        myDialog.setContentView(R.layout.activity_register);
-        txtClose = myDialog.findViewById(R.id.txtClose);
-        txtClose.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                myDialog.dismiss();
-            }
-        });
-        myDialog.show();
-    }
-
     public void onClickRegister(View view) {
         mAuth = FirebaseAuth.getInstance();
-
-        if (rUsername.length() > 0 && rPassword.length() > 0) {
-            mAuth.createUserWithEmailAndPassword(rUsername.getText().toString(), rPassword.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+        if (username.length() > 0 && password.length() > 0) {
+            mAuth.createUserWithEmailAndPassword(username.getText().toString(), password.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
                     if (task.isSuccessful()) {
                         FirebaseDatabase.getInstance().getReference().child("users").child(task.getResult().getUser().getUid()).setValue("Empty");
                         Toast toast = Toast.makeText(getApplicationContext(), "Sign up complete!", Toast.LENGTH_SHORT);
                         toast.show();
-                        myDialog.dismiss();
                     } else {
                         Toast toast = Toast.makeText(getApplicationContext(), "Invalid Email or Password. Please try again.", Toast.LENGTH_SHORT);
                         toast.show();
@@ -132,4 +101,7 @@ public class LoginActivity extends AppCompatActivity {
             });
         }
     }
+
+
+
 }
